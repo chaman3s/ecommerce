f1e497445735d5ddec3302476057a15dbb3e4024
chaman3s <chamanggarwal@gmail.com>
2025-12-09 21:09:13 +0530

    update package.json

--- backend/api/graphql.js
+++ backend/api/graphql.js
@@ -1,6 +1,6 @@
 import { ApolloServer } from "apollo-server-micro";
 import connectDB from "../utils/db.js";
-import typeDefs from "../schema/index.js";
+import typeDefs from "../schema/typeDefs.js";
 import resolvers from "../resolvers/index.js";
 
 let dbConnected = false;
